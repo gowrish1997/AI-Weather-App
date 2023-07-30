7bdad3f2f6bdb8f96cad3bad0718b7cf2b8c28c2
Senthil Kumar <kotarigowrish@gmail.com>
2023-07-30 13:04:54 +0530

    added url

--- lib/getBasePath.ts
+++ lib/getBasePath.ts
@@ -1,6 +1,6 @@
 const getBasePath = () => {
   let base_url =
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
+    process.env.NODE_ENV === "production" ? "https://ai-weather-liart.vercel.app" : "http://localhost:3001";
   return base_url;
 };
 export default getBasePath;
